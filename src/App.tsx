fe2208f7221fce7c587f58e3d57bc6b19a10235c
SzFarkas32 <hunpc7@gmail.com>
2026-03-03 13:00:20 +0100

    asd

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,20 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 
+interface Person{
+  id: number;
+  name: string;
+  age: number;
+  description: string;
+  imagePath: string;
+}
+
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-ó
+
     </>
   )
 }
